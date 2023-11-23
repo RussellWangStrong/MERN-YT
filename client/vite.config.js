c7460c61fa644d7b3fe5e7542c56730929ac0d92
Russell Wang <slimssshark@gmail.com>
2023-11-23 11:55:51 +0800

    complete sign up page funcionality

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,15 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // make a proxy for fetch to use http requests
+  server:{
+    proxy:{
+      '/api':{
+        target:'http://localhost:3000',
+        secure:false
+      }
+    }
+  },
+
   plugins: [react()],
 })
