8ea20117e2ce38dea702e86e8ff93c036d33dd65
Russell Wang <slimssshark@gmail.com>
2023-11-24 12:20:13 +0800

    add redux-persist

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,11 +1,27 @@
-import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit'
+import { combineReducers, configureStore, getDefaultMiddleware } from '@reduxjs/toolkit'
 import  userReducer from './user/userSlice'
+import {persistReducer,persistStore} from 'redux-persist';
+import storage from 'redux-persist/lib/storage'
+
+const rootReducer = combineReducers({user:userReducer});
+
+const persistCofig = {
+    key:'root',
+    storage,
+    version:1,
+}
+// save the user info into the localstorage to prevent the situation that losing the info
+// when you flush your browser
+const persistedReducer = persistReducer(persistCofig,rootReducer)
 
 export const store = configureStore({
-  reducer: {user:userReducer},
-  
+  reducer: persistedReducer,
+
   middleware : (getDefaultMiddleware) => 
   getDefaultMiddleware({
     serializableCheck: false,
   }),
 })
+
+export const persistor = persistStore(store);
+
