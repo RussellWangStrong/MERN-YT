94da557037cf5f38de9c0dcc2a90c51836ca295d
Russell Wang <slimssshark@gmail.com>
2023-11-25 21:17:28 +0800

    make profile private

--- client/src/components/PrivateRouter.jsx
+++ client/src/components/PrivateRouter.jsx
@@ -2,7 +2,7 @@ import {useSelector} from 'react-redux'
 import { Navigate, Outlet } from 'react-router-dom'
 
 export default function PrivateRouter() {
-
+// todo ： how to comprehend it？ 
     const {currentUser} = useSelector((state) => state.user)
 
   return currentUser ? <Outlet/> : <Navigate to='/sign-in'/>
