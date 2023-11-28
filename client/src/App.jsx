3cd79ef703709487e0a111196036bd97ecba4c42
Russell Wang <slimssshark@gmail.com>
2023-11-28 12:05:03 +0800

    complete create listing page UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import About from './pages/About';
 import Profile from './pages/Profile';
 import Header from './components/Header';
 import PrivateRouter from './components/PrivateRouter';
+import CreateListing from './pages/CreateListing';
 
 export default function App() {
   return (
@@ -16,8 +17,10 @@ export default function App() {
         <Route path='/sign-in' element={<SignIn />} />
         <Route path='/sign-up' element={<SignUp />} />
         <Route path='/about' element={<About />} />
+        {/* need to authenticate */}
         <Route element={<PrivateRouter />}>
           <Route path='/profile' element={<Profile />} />
+          <Route path='/create-listing' element={<CreateListing />} />
         </Route>
       </Routes>
     </BrowserRouter>
